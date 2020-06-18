e5d1b4bc733f314bcaf712cfb30950834ba47633
PIerluigi <895353+Pierluigit@users.noreply.github.com>
2020-06-18 13:53:10 +0200

    update share pdf

--- scripts/inc.footer.php
+++ scripts/inc.footer.php
@@ -9,13 +9,15 @@
 	</div>
 	<div class="coming-soon-copyright">
 		<?php if($okSettings==true) {?>
-			App <?php echo($app_nameProject);?> made with <i class="fas fa-heartbeat"></i> <!--©--> <?php echo(date('Y'));?>!<br>
+			<?php echo($app_nameProject);?> app made with <i class="fas fa-heartbeat"></i> Heart Chakra <!--©--> <?php echo(date('Y'));?>!<br>
 			<a href="connect.php">Connect</a> - <?php if($app_ifBlockNewRegistration!="yes") {?><a href="register.php">Register</a> -<?php }?> <a href="lostPassword.php">Lost Password</a>
 		<?php if($app_ifOnlyUseApp!="yes") {?>
 		- <a href="privacy.php">Privacy</a> - <a href="terms.php">Terms</a> - <a href="faq.php">F.A.Q.</a> - <a href="contact.php">Contact</a> - <a href="404.php">404</a>
 		<?php }?>
-		<?php if(file_exists('infos/install.php')) {?>- <a href="infos/install.html">Read Me</a><?php }?>
+		<?php if(file_exists('infos/install.html')) {?>- <a href="infos/install.html">Read Me</a><?php }?>
 		<?php }?>
+		<br>
+		PS : Nous soutenons la lutte contre la Pédocriminalité <a href="https://teamfsociety.com/">Team Fsociety</a>
 	</div>
 </div>
 <!-- END coming-soon-footer -->
